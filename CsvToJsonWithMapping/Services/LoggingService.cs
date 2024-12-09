4b8596a483e9a168c4b5e42fe63ea71a95ddb57b
Jochem <jochem.kleverwal@gmail.com>
2024-12-09 22:49:44 +0100

    LoggingService

---
+++ CsvToJsonWithMapping/Services/LoggingService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvToJsonWithMapping.Services
+{
+    public class LoggingService
+    {
+        private readonly Dictionary<string, List<string>> _log = new();
+
+        public void Log(string type, string message)
+        {
+            if (!_log.ContainsKey(type))
+            {
+                _log[type] = new List<string>();
+            }
+
+            _log[type].Add(message);
+        }
+
+        public Dictionary<string, List<string>> GetLogs()
+        {
+            return _log;
+        }
+
+        public void ClearLogs()
+        {
+            _log.Clear();
+        }
+    }
+}
